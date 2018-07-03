cbce1f4fac499f14a58e2434550795874e942a2e
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2018-07-02 21:53:46 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,18 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+		var nombre;
+		//nombre="Adrián";
+		//alert(nombre);
+		nombre=prompt("Ingrese su nombre", "natalia natalia");
+		alert("Su nombre es:"+ nombre);
+
+		 //Comentario de una línea//
+	     //*Comentario de la clase*/
+	     //var person = prompt("Please enter your name", "Harry Potter");
+
+
+}
+
+
