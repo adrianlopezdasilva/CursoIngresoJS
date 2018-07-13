ce661b64db5572a1e15be4337783d5daab4dba01
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2018-07-12 22:06:28 -0300

    Add files via upload

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+
+	var num;
+
+	num=Math.floor(Math.random()*(10)+1;
+
+	if(num>=9)	{
+
+	alert("EXCELENTE")
+
+	}
+    
+    else(notas>=4)
+	
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 ........   
+
+	/* var num;
+	   num=Math.floor.(Math.random)*(max-min)+min;         [min,max)*/
+    
+	   var num;
+
+       num=Math.floor(Math.random()*(10)+1;
+
+       alert(num);
+
+   
+}//FIN DE LA FUNCIÓN
