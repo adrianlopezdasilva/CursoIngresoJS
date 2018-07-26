db91df7e3ff5dca3d3cbe6838f8ad932c8a398f8
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2018-07-26 16:44:52 -0300

    Add files via upload

--- jsInstruccionSwitch(3).js
+++ jsInstruccionSwitch(3).js
@@ -3,17 +3,16 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	alert (mesDelAño);
 	
-	
+	switch(mesDelAño){
+		case"Febrero":
+		alert("Este mes no tiene más de 29 días.");
+		break;
+
+		default:
+		alert("Este mes tiene 30 o más días");
+	}
 
 
 }//FIN DE LA FUNCIÓN
-
-
-/*
-e/s 1-10
-if  1-0
-switch	1-6
-parcial 1-3 -> 4 con if, 5 con switch
-recu 1-3 ->  4 if, 5 switch
