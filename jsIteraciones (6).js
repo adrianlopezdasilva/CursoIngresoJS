f2e1db768a1366e24d9ea3fe59b144829496e413
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2018-07-26 21:16:44 -0300

    Add files via upload

---
+++ jsIteraciones (6).js
@@ -0,0 +1,24 @@
+function mostrar()
+{
+	var numero;
+	var contador=0;
+	var acumulador=0;
+
+
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/5;
+
+while(contador<5){
+	contador++;
+	numero=prompt("Ingrese un núnero.");
+	numero=parseInt(numero);
+	suma+=numero; //suma=suma+5;
+	promedio=suma/contador;
+
+
+
+
+
+}
+}//FIN DE LA FUNCIÓN
---
+++ jsIteraciones (7).js
@@ -0,0 +1,35 @@
+function mostrar()
+{
+	var numero;
+	var respuesta="si";
+	var contador=0;
+	var acumulador=0;
+	var respuesta='si';
+
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/contador;
+
+	while(true){
+
+	numero=prompt("Desea parar?");
+	if(numero="si");
+	break;
+	continue;	
+
+
+	}
+	numero=parseInt(numero);
+	contador++
+
+}//FIN DE LA FUNCIÓN
+
+
+/* 
+    while(isnan(num)){
+	num=parseInt
+   num=parseint(num);||NaN
+   if(isnan(num)){
+	break;
+   }
+   }
