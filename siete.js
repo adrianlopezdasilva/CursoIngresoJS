731109ef798403cafe8d86368a5c0596844a05fc
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2018-08-02 19:13:08 -0300

    Add files via upload

---
+++ siete.js
@@ -0,0 +1,40 @@
+function mostrar()
+{
+	if(contador==0){
+
+		edadMax=edad;
+		edadMin=edad;
+		nombreMayor=nombre;s
+
+	}else {
+
+		if(edadMax<edad){
+
+			edadMax=edad
+			nombreMayor=nombre;
+
+			 if(sexo="m"){}
+		}
+
+		if(edadMin>edad){
+
+			edadMin=edad;
+		}
+
+
+	}
+
+	if(sexo="f"){
+
+		 if(edad>edadMaxFem){
+
+		 	edadMaxFem=edad;
+		 	nombreFemMasVieja=nombre;
+		 }
+	}else
+
+
+	
+
+
+}
